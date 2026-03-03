8b9060c87be338d21fd53b24f6343e344353410e
Bruno Bandeira <brunohenrique0665@gmail.com>
2026-03-03 18:48:10 -0300

    first commit

---
+++ src/main/java/com/FalaPraEle/FalaPraEleApplication.java
@@ -0,0 +1,13 @@
+package com.FalaPraEle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaPraEleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaPraEleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FalaPraEle/FalaPraEleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FalaPraEle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalaPraEleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
